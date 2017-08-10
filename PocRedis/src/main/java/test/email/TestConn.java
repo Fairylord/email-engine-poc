15c3055ca9aec78c3a2fc70d1111089fb187c713
yellowb <122517167@qq.com>
2017-08-10 13:49:19 +0800

    add javamail

---
+++ PocRedis/src/main/java/test/email/TestConn.java
@@ -0,0 +1,20 @@
+package test.email;
+
+
+import java.util.Properties;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
+
+public class TestConn {
+    public static void main(String[] args) {
+
+    }
+
+    public static void getProperties() {
+        Properties p = System.getProperties();
+    }
+}
