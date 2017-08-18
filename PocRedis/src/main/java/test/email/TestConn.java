463a5b9fa74b1c5fd00958830c0a3410e48b0fcf
yellowb <122517167@qq.com>
2017-08-18 08:34:54 +0800

    add javamail 1.6

--- PocRedis/src/main/java/test/email/TestConn.java
+++ PocRedis/src/main/java/test/email/TestConn.java
@@ -9,12 +9,23 @@ import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Store;
 
+import com.sun.mail.imap.IMAPFolder;
+import com.sun.mail.imap.IMAPMessage;
+
 public class TestConn {
     public static void main(String[] args) {
 
+        Properties properties = getProperties();
+
+
+
     }
 
-    public static void getProperties() {
+    public static Properties getProperties() {
         Properties p = System.getProperties();
+        p.setProperty("mail.store.protocol", "imap");
+        p.setProperty("mail.imap.timeout", "300000");// 5 mins
+        p.setProperty("mail.imap.connectiontimeout", "600000");// 10 mins
+        return p;
     }
 }
